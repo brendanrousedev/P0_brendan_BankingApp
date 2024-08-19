e5d56905a0fba1a6af82ecc3300abbef90eb71c2
brendanrousedev <brouse76@gmail.com>
2024-08-19 15:00:35 -0600

    Added blank files

--- Program.cs
+++ Program.cs
@@ -3,60 +3,18 @@
 // PASSWORD: password123
 
 using System;
+using System.ComponentModel.Design;
 using System.Security.Cryptography;
 using System.Text;
 
 public class Program
 {
+
+    Menu CustomerMenu = new Menu("customer.json");
     public static void Main(string[] args)
     {
-        // Define your connection string here
-        
+    
     }
+    
 
-    public static string HashPassword(string password, byte[] salt)
-    {
-        // implements Hash-based message authentication code using SHA-512 hashing algorithm
-        using (var hmac = new HMACSHA512(salt))
-        {
-            var passwordBytes = System.Text.Encoding.UTF8.GetBytes(password);
-            var hashBytes = hmac.ComputeHash(passwordBytes);
-            return Convert.ToBase64String(hashBytes);
-        }
-    }
-
-    public static byte[] GenerateSalt()
-    {
-        // using - resources are disposed of after being used. RNGCryptoServiceProvider will be dropped after it is used
-        // implicity declare the type of variable
-        using (var rng = new RNGCryptoServiceProvider())
-        {
-            // set the variable salt to a byte-array
-            var salt = new byte[16]; // 128-bit salt
-            // fill the byte array 
-            rng.GetBytes(salt);
-            return salt;
-        }
-    }
-
-    public static bool VerifyUser(string username, string password)
-    {
-        using (var context = new P0BrendanBankingDbContext())
-        {
-            var user = context.Admins.SingleOrDefault(a => a.AdminUsername == username);
-
-            if (user == null)
-            {
-                return false; // User not found
-            }
-
-            return VerifyPassword(password, user.PasswordHash, user.Salt);
-        }
-    }
-
-    public static bool VerifyPassword(string enteredPassword, string storedHash, byte[] storedSalt)
-    {
-        var hashOfEnteredPassword = HashPassword(enteredPassword, storedSalt);
-        return hashOfEnteredPassword == storedHash;
-    }
 }
--- README.md
+++ README.md
@@ -4,6 +4,9 @@ CURRENT TASKS:
 
 
 TODO:
-1. Need to figure out how to build a menu from a json
-2. Need to implement the password hasing and salting methods for when new users and admins are created
-3. Need to implement skeleton
+Need to figure out how to build a menu from a json
+Need to implement skeleton
+Add Transaction table to DB - How will I alter the db context? 
+
+Transaction Columns:
+PK_transactionId, FK_accountId, type (withdraw, deposit, transfer), money, date
