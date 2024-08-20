17452d965cfef21bd9329a6b8ec08c1f311ed6b9
brendanrousedev <brouse76@gmail.com>
2024-08-20 00:06:51 -0600

    Added comment to top of Program.cs

--- Program.cs
+++ Program.cs
@@ -3,6 +3,18 @@ using System.Collections.Generic;
 using System.IO;
 using Newtonsoft.Json;
 
+/******************************************************************************
+* Author: Brendan Rouse
+* 
+* 1. Initially, there was one problem that had to be solved before the application
+* could be written. In order to add new customers, and new admins, at least one
+* admin must already exist in the DB. While it is considered poor practice to
+* explicitly show a username and password in the source code, for the purposes of 
+* this project, I decided it was necessary to list the username and password below
+* for reference, so a user can sign in as a Admin and begin performing necessary 
+* operations to perform DB operations. 
+*******************************************************************************/
+
 public class Program
 {
 
--- README.md
+++ README.md
@@ -6,6 +6,7 @@ CURRENT TASKS:
 TODO:
 Need to implement skeleton
 Add Transaction table to DB - How will I alter the db context.cs? 
+Add the ability for an admin to create other admins.
 
 Transaction Columns:
 PK_transactionId, FK_accountId, type (withdraw, deposit, transfer), money, date
