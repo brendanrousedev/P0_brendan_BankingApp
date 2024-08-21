dd645306ee2eea4a3aac60bd0e28d3fee0000b3f
brendanrousedev <brendanrousedev@gmail.com>
2024-08-20 21:18:30 -0600

    Fixed Confirm function so it does not print the messages on both lines

--- Util/IOConsole.cs
+++ Util/IOConsole.cs
@@ -193,7 +193,7 @@ ______             _             __    ___           _        _       _
     public bool Confirm(string message)
     {
         NewLine();
-        Console.Write(message); 
+        Console.WriteLine(message); 
         Console.Write("Enter 'y' for Yes, any other key for No): ");
         string? option = Console.ReadLine()?.Trim().ToLower();
         if (string.IsNullOrEmpty(option))
