3bf96b40548b108e3d7e82594798ee5c8f868897
brendanrousedev <brendanrousedev@gmail.com>
2024-08-22 15:37:07 -0600

    Added Spectre package

--- obj/Debug/net8.0/P0_brendan_BankingApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/P0_brendan_BankingApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("P0_brendan_BankingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac2dab8120629ba7413f737824825d7051ff1ee3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e53f0daf8d450870a22c1ab0a344558e29f8d88")]
 [assembly: System.Reflection.AssemblyProductAttribute("P0_brendan_BankingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("P0_brendan_BankingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
